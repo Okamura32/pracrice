6e3fbfa1a67d423dc102a2f3abd1fdb6feffe64d
Okamura32 <61731507+Okamura32@users.noreply.github.com>
2020-04-09 18:28:33 +0900

    inital commit

--- beautiful_soup.py
+++ beautiful_soup.py
@@ -36,9 +36,38 @@ def Weather():
 
   l=[]
   ans="あいうえお"
-  for i in range(len(time)):
-    l.append(str(time[i])+":"+"天気は"+str(weth[i])+":"+"気温は"+str(temp[i])+"℃")
+  # for i in range(len(time)):
+  #   l.append(str(time[i])+":"+"天気は"+str(weth[i])+":"+"気温は"+str(temp[i])+"℃")
     # print(time[i],":"+" 天気は",weth[i],":","気温 は",temp[i],"℃")
+  max_temp=max(temp)
+  min_temp=min(temp)
+  cloud=weth.count("曇り")
+  sunny=weth.count("晴れ")
+  rainy=weth.count("雨")
+  max_weth=max(cloud,sunny,rainy)
+
+  today=""
+  count=0
+  if cloud==max_weth:
+    today="曇り"
+    count=1
+  if sunny==max_weth:
+    if count==1:
+      today+="または晴れ"
+    else:
+      today="晴れ"
+      count=1
+  if rainy==max_weth:
+    if count==1:
+      today+="雨"
+    else:
+      today="雨"
+
+  
+  
+  
+  ans="最高気温は"+str(max_temp)+"℃ "+" 最低気温は"+str(min_temp)+"℃ "+" 多分"+today
   return ans
+
 
   
